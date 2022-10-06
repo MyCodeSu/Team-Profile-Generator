864e78572a245fe043379ac1978a4321ea364a88
michaelsu01@gmail.com <michaelsu01@gmail.com>
2022-10-06 01:27:58 -0400

    commit?

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,20 @@
+const Employee = require('../lib/Employee');
+
+test('Create new Manager', () => {
+    const employee = new Manager('Gender Neutral Name', 246, 'someone@email.com', 1234567890);
+
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+    expect(employee.officeNumber).toEqual(expect.any(Number));
+})
+
+test('Checks every method in the Manager class', () => {
+    const employee = new Manager('Gender Neutral Name', 246, 'someone@email.com', 1234567890);
+
+    expect(employee.getName()).toBe(employee.name);
+    expect(employee.getId()).toBe(employee.id);
+    expect(employee.getEmail()).toBe(employee.email);
+    expect(employee.getOfficeNumber()).toBe(employee.officeNumber);
+    expect(employee.getRole()).toBe('Manager');
+})
